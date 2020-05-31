1fd746316b0e672ef2966559bc02dd7d9f139d9c
João Henrique <joaohenrique.hs@hotmail.com>
2020-05-31 15:34:56 -0300

    feat: Add more c_flags to ensure raw mode

--- meown.c
+++ meown.c
@@ -6,6 +6,11 @@
 
 struct termios orig_termios;
 
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode() { 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); 
 }
@@ -14,19 +19,31 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+
+  raw.c_iflag &= ~( BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag &= ~(CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 100;
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
   enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
+
+    if (c == 'q') break;
   };
   return 0;
 }
