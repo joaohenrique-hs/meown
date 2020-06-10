ddcf43757406e006178c38ddae5cc85238aba0fa
João Henrique <joaohenrique.hs@hotmail.com>
2020-06-10 15:34:31 -0300

    feat: Changed exit key to Ctrl+Q

--- meown.c
+++ meown.c
@@ -6,7 +6,8 @@
 #include <termios.h>
 #include <unistd.h>
 
-/******/
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
 struct termios orig_termios;
@@ -55,7 +56,7 @@ int main() {
       printf("%d ('%c')\r\n", c, c);
     }
 
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q')) break;
   };
   return 0;
 }
