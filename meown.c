7bf26542d436990c6ff16755ac5659d160edf92f
João Henrique <joaohenrique.hs@hotmail.com>
2020-06-20 13:26:16 -0300

    feat: WIP add cursor update

--- meown.c
+++ meown.c
@@ -17,6 +17,7 @@
 /*** data ***/
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios; // struct to receive original terminal atributes
@@ -137,7 +138,10 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
@@ -167,6 +171,9 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
@@ -177,6 +184,7 @@ int main() {
   while (1) {
     editorRefreshScreen();
     editorProcessKeypress();
+    E.cx ++;
   };
   return 0;
 }
