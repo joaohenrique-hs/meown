3463e80aa2fe9c976e646b85fec43c22c7d9e496
João Henrique <joaohenrique.hs@hotmail.com>
2020-06-27 15:21:01 -0300

    feat: Add open files

--- meown.c
+++ meown.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include "sys/types.h"
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
@@ -28,10 +29,17 @@ enum editorKey {
 
 /*** data ***/
 
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 struct editorConfig {
   int cx, cy;
   int screenrows;
   int screencols;
+  int numrows;
+  erow row;
   struct termios orig_termios;
 };
 
@@ -126,6 +134,30 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** file i/o ***/
+
+void editorOpen (char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if(!fp) die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen -1] == '\n' || line[linelen -1] == '\r')) 
+        linelen--;
+
+      E.row.size = linelen;
+      E.row.chars = malloc(linelen + 1);
+      memcpy(E.row.chars, line, linelen);
+      E.row.chars[linelen] = '\0';
+      E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
+}
+
 /*** append buffer ***/
 
 struct abuf {
@@ -153,26 +185,32 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-          "Meown editor -- version %s", MEOWN_VERSION);
-      if (welcomelen > E.screencols) welcomelen = E.screencols;
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
+    if(y >= E.numrows) {
+      if (y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+            "Meown editor -- version %s", MEOWN_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+        }
+        while (padding--) {
+          abAppend(ab, " ", 1);
+        }
+        abAppend(ab, welcome, welcomelen);
+      } else {
         abAppend(ab, "~", 1);
       }
-      while (padding--) {
-        abAppend(ab, " ", 1);
-      }
-      abAppend(ab, welcome, welcomelen);
-    } else {
-      abAppend(ab, "~", 1);
-    }
 
-    abAppend(ab, "\x1b[K", 3);
-    if (y < E.screenrows -1) {
-      abAppend(ab, "\r\n", 2);
+      abAppend(ab, "\x1b[K", 3);
+      if (y < E.screenrows -1) {
+        abAppend(ab, "\r\n", 2);
+      }
+    } else {
+      int len = E.row.size;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab, E.row.chars, len);
     }
   }
 }
@@ -259,13 +297,17 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.numrows = 0;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
+  if (argc >= 2){
+    editorOpen(argv[1]);
+  }
 
   while (1) {
     editorRefreshScreen();
